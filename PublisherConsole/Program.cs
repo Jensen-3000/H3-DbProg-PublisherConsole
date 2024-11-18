b8b2601e7671e9af865e5835ac61fbaed970655d
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-11-18 12:15:50 +0100

    feat: add lil'Palles extra methods

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using PublisherData;
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherData;
 using PublisherDomain;
 
 
@@ -27,5 +28,39 @@ void GetAuthors()
     foreach (var author in authors)
     {
         Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
+
+void AddAuthorWithBook()
+{
+    var author = new Author { FirstName = "Julie", LastName = "Lerman" };
+    author.Books.Add(new Book
+    {
+        Title = "Programming Entity Framework",
+        PublishDate = new DateOnly(2009, 1, 1)
+    });
+
+    author.Books.Add(new Book
+    {
+        Title = "Programming Entity Framework 2nd Ed",
+        PublishDate = new DateOnly(2010, 8, 1)
+    });
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthorsWithBooks()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.Include(a => a.Books).ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine(book.Title);
+        }
+
     }
 }
